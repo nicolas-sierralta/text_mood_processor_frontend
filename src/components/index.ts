d64fa3072eb229e44902441a946aad7cc9d8c6bf
nicolas-sierralta <nicolas-sierralta@users.noreply.github.com>
2024-10-01 16:47:41 +0000

    deploy: e6579e615229e0739016140adaaaec6ac3eeb0de

--- src/components/index.ts
+++
@@ -1,8 +0,0 @@
-export { default as AnalyzeButton } from './AnaylzeButton/AnalyzeButton';
-export { default as ChangeToneButton } from './ChangeToneButton/ChangeToneButton';
-export { default as TextAreaInput } from './TextAreaInput/TextAreaInput';
-export { default as ToneSelector } from './ToneSelector/ToneSelector';
-export { default as AnalyzeResultDisplay } from './AnalyzeResultDisplay/AnalyzeResultDisplay';
-export { default as ChangeToneResultDisplay } from './ChangeToneResultDisplay/ChangeToneResultDisplay';
-
-
--- src/hooks/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as useAnalyzeTone } from './useAnalyzeTone';
-export { default as useChangeTone } from './useChangeTone';
-export { default as useTextMoodProcessor } from './useTextMoodProcessor';
